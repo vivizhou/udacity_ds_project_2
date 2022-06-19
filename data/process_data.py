aacb66e55f66fc67c3641fcf8161c78eacc75100
Wei Zhou <vivizhouxw02@gmail.com>
2022-06-19 15:16:01 +0200

    ETL

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,41 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on = 'id')
+    return(df)
 
 def clean_data(df):
-    pass
-
+    
+    categories = df['categories'].str.split(';', expand = True)
+    
+    row = categories.iloc[1,:]
+    
+    category_colnames = row.apply(lambda string: string[0:-2])
+    
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda string: string[-1])
+    
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+        
+        
+    df = df.drop(['categories'], axis = 1)
+    df = pd.concat([df, categories], axis = 1)
+    df.drop_duplicates(inplace=True)
+    return(df)
 
 def save_data(df, database_filename):
-    pass  
+    path = 'sqlite:///' + database_filename
+    engine = create_engine(path)
+    df.to_sql(database_filename, engine, index=False)
 
 
 def main():
